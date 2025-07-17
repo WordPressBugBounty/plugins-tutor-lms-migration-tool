27b7e25974b24d3e332c0151bb21c9d8d70168ec
Sean Murphy <sean@iamseanmurphy.com>
2025-07-17 06:18:50 +0000

    2.3.1

---
+++ tutor-lms-migration-tool/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themeum/tutor-lms-migration-tool',
+        'pretty_version' => '2.3.1',
+        'version' => '2.3.1.0',
+        'reference' => '39a5a88c3a7901113390ba6827cf4cea7e7e587f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'themeum/tutor-lms-migration-tool' => array(
+            'pretty_version' => '2.3.1',
+            'version' => '2.3.1.0',
+            'reference' => '39a5a88c3a7901113390ba6827cf4cea7e7e587f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
